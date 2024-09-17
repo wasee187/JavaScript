6739d202957f3c20285c24026fd07447693c680f
wasee187 <waseesarwar187@gmail.com>
2024-09-17 23:55:51 +0300

    array_exercise_4

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const numbers = [1,2,3,4,5];
 
 
-const output = move(numbers, 0,1);
+const output = move(numbers, 2,-3);
 
 console.log(output);
 
 function move(array, index, offset){
-    
+    const position = index + offset;
+    if(position>=array.lenght || position < 0){
+        console.error("Invalid offset.");
+    }else{
+        let output = [...array];
+        const element = output.splice(index,1)[0];
+        output.splice(position,0, element)
+        return output;
+    } 
+
 }
